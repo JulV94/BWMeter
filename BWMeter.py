c4a44749c4c52d6ec7aa1021a587c75b52735793
JulV <julvdev@gmail.com>
2016-09-25 16:49:22 +0000

    add html files

--- BWMeter.py
+++ BWMeter.py
@@ -3,34 +3,30 @@
 # Bandwidth meter program
 
 import subprocess as sp
-from plotly.offline import plot
-from plotly.graph_objs import Bar, Scatter, Figure, Layout
+import MySQLdb as sql
+import json
 
 
 def take_measurement():
     result = sp.Popen("speedtest-cli-extras/bin/speedtest-csv", stdout=sp.PIPE).stdout.read().decode("utf-8").split(";")
-    ping = result[6].split(" ")[0]
-    down = result[7].split(" ")[0]
-    up = result[8].split(" ")[0]
 
-    print(ping)
-    print(down)
-    print(up)
+    with open('config/config.json') as config_file:
+        config = json.load(config_file)
 
-    plot([Scatter(x=[1, 2, 3], y=[ping, down, up])], filename="html/test.html", auto_open=False)
-
-# put the data in the SQL database
-
-
-def generate_graphs_html():
-    pass
-# trigger request on SQL database
-
-# Generating graphs
+    db = sql.connect(
+        host=config['db_host'], user=config['db_username'], passwd=config['db_password'], db=config['db_name'])
+    cur = db.cursor()
+    try:
+        cur.execute(
+            "INSERT INTO " + config['db_table_name'] + " VALUES ( NOW(),"
+            + result[6].split(" ")[0] + "," + result[7].split(" ")[0] + "," + result[8].split(" ")[0] + ")")
+        db.commit()
+    except:
+        db.rollback()
+        print("Cannot write into " + config['db_name'] + "db table " + config['db_table_name'])
+    db.close()
 
 if __name__ == '__main__':
     print("Taking measurement...")
     take_measurement()
-    print("Generating graphs...")
-    generate_graphs_html()
     print("Completed")
---
+++ setup.py
@@ -0,0 +1,24 @@
+# Setup for the BWMeter.py script
+
+import MySQLdb as sql
+import json
+
+
+def init_db():
+    with open('config/config.json') as config_file:
+        config = json.load(config_file)
+    db = sql.connect(host=config['db_host'], user=config['db_username'], passwd=config['db_password'])
+    cur = db.cursor()
+    cur.execute("CREATE DATABASE "+config['db_name']+";")
+    db.close()
+    db = sql.connect(
+        host=config['db_host'], user=config['db_username'], passwd=config['db_password'], db=config['db_name'])
+    cur = db.cursor()
+    cur.execute(
+        "CREATE TABLE "+config['db_table_name']+" (measurement_time datetime,ping float,down float, up float );")
+    db.close()
+
+if __name__ == "__main__":
+    print("Creating db...")
+    init_db()
+    print("Complete")
